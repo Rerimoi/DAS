248239975d5e73dd7d6f6bd839254fb8c011d335
Mwende <josefridahmwende71@gmail.com>
2018-11-06 20:25:10 -0800

    added /lecturer route

--- routes/index.js
+++ routes/index.js
@@ -7,7 +7,7 @@ router.get('/login', function(req,res){resc.render('login', {title: 'Login page'
 // router.get('/login/student', function(req,res){res.render('index', {title: 'Login student page'})})
 // router.get('/signup/lecturer', function(req,res){res.render('index', {title: 'Signup lecturer page'})})
 // router.get('/singup/student', function(req,res){res.render('index', {title: 'Login student page'})})
-// router.get('/lecturer', function(req,res){res.render('index', {title: 'Welcome Lecturer'})})
+router.get('/lecturer', function(req,res){res.render('index', {title: 'Welcome Lecturer'})})
 router.get('/student', function(req,res){res.render('index', {title: 'Welcome student'})})
 
 module.exports = router
