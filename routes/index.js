3b55ae8f703deaeb7c95b77fe27b8939def6093d
Rerimoi <prkwonyike@gmail.com>
2019-01-20 00:55:04 +0300

    changed routing of the signup page

--- routes/index.js
+++ routes/index.js
@@ -4,8 +4,9 @@ const router = express.Router()
 
 // pages routes
 router.get('/', function(req,res){res.render('home', {title: 'Class Attendance Tracking System'})})
-
-router.get('/login', function(req,res){res.render('login', {title: 'Login page'})})
+//dashboard route
+router.get('/dashboard', function(req,res){res.render('dashboard',{title: 'Class Attendance System'})})
+router.get('/signin', function(req,res){res.render('signin', {title: 'Login page'})})
 // router.get('/login/student', function(req,res){res.render('index', {title: 'Login student page'})})
 // router.get('/signup/lecturer', function(req,res){res.render('index', {title: 'Signup lecturer page'})})
 // router.get('/singup/student', function(req,res){res.render('index', {title: 'Login student page'})})
